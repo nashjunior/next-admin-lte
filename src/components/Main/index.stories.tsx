0810e1a36a1f153e1677268d9c00984a7ba44da6
nashjunior <nashjuniorjj@gmail.com>
2021-08-10 22:20:26 -0300

    refactor: storybook should use knobs

--- src/components/Main/index.stories.tsx
+++ src/components/Main/index.stories.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import Main from '.';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: 'Main',
-  componenet: Main,
+  component: Main,
+  decorators: [withKnobs],
 };
 
 // eslint-disable-next-line no-undef
-export const Basic = (): JSX.Element => <Main />;
+export const Basic = (): JSX.Element => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text(
+      'Descriptio',
+      'Typscript, ReactJS, NextJS, Styled Components',
+    )}
+  />
+);
