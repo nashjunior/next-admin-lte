048bd614dddef12a4ef574680ceaa25259e43551
nashjunior <nashjuniorjj@gmail.com>
2021-08-13 20:23:57 -0300

    chore: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)', '!*.stories.(js|jsx|ts|tsx)', '../src/**/*.stories.mdx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   
   
