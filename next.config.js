2fcb0d4aea7a37c3dfdbe9158022f1a0ad2c88a1
nashjunior <nashjuniorjj@gmail.com>
2021-08-13 20:13:30 -0300

    chore: fix next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')
+const withPlugins = require('next-compose-plugins')
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withPlugins([
+ { reactStrictMode: !isProd},
+
+[withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd,
+  }
+})]])
+
