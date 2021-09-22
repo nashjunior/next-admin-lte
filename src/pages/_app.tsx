68923f971f543e38351f2c43475518be5aaf8731
nashjunior <nashjuniorjj@gmail.com>
2021-09-22 20:50:03 -0300

    refactor: should validate window

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,21 +3,27 @@ import Head from 'next/head';
 import GlobalStyles from '../styles/global';
 
 // eslint-disable-next-line no-undef
-const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <>
-    <Head>
-      <title>React Avançado - BoilerPlater</title>
-      <link rel="shortcut icon" href="img/icon-512.png" />
-      <link rel="apple-touch-icon" href="img/icon-192.png" />
-      <link rel="manifest" href="/manifest.json" />
-      <meta
-        name="description"
-        content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-      />
-    </Head>
-    <Component {...pageProps} />
-    <GlobalStyles />
-  </>
-);
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : (
+        <>
+          <Head>
+            <title>React Avançado - BoilerPlater</title>
+            <link rel="shortcut icon" href="img/icon-512.png" />
+            <link rel="apple-touch-icon" href="img/icon-192.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+              name="description"
+              content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+            />
+          </Head>
+          <Component {...pageProps} />
+          <GlobalStyles />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
