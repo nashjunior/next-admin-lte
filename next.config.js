5f4caf75ddd75106323646bc03c2555c742cfe11
nashjunior <nashjuniorjj@gmail.com>
2021-10-26 20:54:23 -0300

    refactor: should support sass

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 const withPWA = require('next-pwa')
 const withPlugins = require('next-compose-plugins')
+const path = require('path')
 
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPlugins([
  { reactStrictMode: !isProd},
 
+ {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+ },
 [withPWA({
   pwa: {
     dest: 'public',
